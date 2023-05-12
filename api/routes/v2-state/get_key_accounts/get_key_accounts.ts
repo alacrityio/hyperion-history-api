d9283fd57613ad9393b27470d2e3c42ea4c5906b
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2023-05-12 17:48:38 -0600

    perf(api): use alaiojs library for get_key_accounts endpoint

--- api/routes/v2-state/get_key_accounts/get_key_accounts.ts
+++ api/routes/v2-state/get_key_accounts/get_key_accounts.ts
@@ -1,6 +1,6 @@
 import {FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
 import {timedQuery} from "../../../helpers/functions";
-import {Numeric} from "eosjs/dist";
+import {Numeric} from "alaiojs/dist";
 import {getSkipLimit} from "../../v2-history/get_actions/functions";
 
 function invalidKey() {
